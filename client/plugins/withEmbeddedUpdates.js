9143596881dda0878170806d60aedae63a026e07
Paul Shippy <pshippy@gmail.com>
2026-08-25 01:53:58 +0000

    Force embedded JavaScript bundle in iOS builds

---
+++ client/plugins/withEmbeddedUpdates.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const { withDangerousMod } = require('@expo/config-plugins');
+
+module.exports = function withEmbeddedUpdates(config) {
+  return withDangerousMod(config, [
+    'ios',
+    async (config) => {
+      const projectRoot = config.modRequest.platformProjectRoot;
+      const projectName = config.modRequest.projectName;
+      const projectFile = path.join(projectRoot, `${projectName}.xcodeproj`, 'project.pbxproj');
+      const project = fs.readFileSync(projectFile, 'utf8');
+
+      fs.writeFileSync(projectFile, project.replace(/SKIP_BUNDLING/g, 'FORCE_BUNDLING'));
+      return config;
+    },
+  ]);
+};
